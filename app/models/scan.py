91aa2c7add49972d6323aacfdb18e6df1e595ac1
Taiwo Adebisi <adebisijosephh@gmail.com>
2023-04-04 22:08:32 +0100

    Implement Scan Ednpoints

--- app/models/scan.py
+++ app/models/scan.py
@@ -1,6 +1,6 @@
 from typing import List
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from pytz import timezone
 from .user import User
@@ -13,7 +13,8 @@ class ScanType(str, Enum):
 
 
 class Scan(BaseModel):
-    id: str
+    id: str = Field(default=None, alias="_id") 
+    user_id: str = None
     type: ScanType
     exchangeA: str
     exhangeB: str
@@ -24,4 +25,12 @@ class Scan(BaseModel):
     intervals: str = None
     active: bool = False
     timestamp: datetime = datetime.now(timezone("UTC"))
+    
+    class Config:
+        orm_mode = True
+
+
+class ScanUpdate(BaseModel):
+    # startdate: datetime = datetime.now(timezone("UTC"))
+    active: bool = False
 
--- app/routes/scans.py
+++ app/routes/scans.py
@@ -2,9 +2,9 @@ from typing import List
 from bson import ObjectId
 from fastapi import APIRouter, Depends, Request, HTTPException
 from middleware.authMiddleware import authenticate_request
-from ..models.scan import Scan
+from ..models.scan import Scan, ScanUpdate
 from mongodb import db
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 router = APIRouter()
@@ -13,15 +13,18 @@ router = APIRouter()
 @router.post("/")
 async def create_scan(request: Request, scan: Scan) -> Scan:
     user = request.state.user
-    db.scans.insert_one({"user_id": user.id, **scan.dict()})
-    return Scan(user_id=user.id, **scan.dict())
+    scan.user_id = user.id
+    scan_db = db.scans.insert_one(scan.dict())
+    return scan.copy(update={
+        "_id": str(scan_db.inserted_id)
+    })
 
 
 @router.get("/")
 async def get_scans(request: Request) -> List[Scan]:
-    print(ObjectId(request.state.user.id))
     scans = db.scans.find({'user_id': request.state.user.id})
-    return [Scan(id=str(scan["_id"]), **scan) for scan in scans]
+
+    return [Scan(**scan) for scan in scans]
 
 
 @router.get("/{scan_id}")
@@ -35,5 +38,14 @@ async def get_scan(request: Request, scan_id: str) -> Scan:
 
 
 @router.patch("/{scan_id}")
-async def pause_scan(request: Request, scan_id: str):
-    pass
+async def pause_scan(request: Request, scan_id: str, scan_update: ScanUpdate):
+    query = {'_id': ObjectId(scan_id), 'user_id': request.state.user.id}
+    scan_db = db.scans.find_one(query)
+    if not scan_db:
+        raise HTTPException(status_code=400, detail="Not Found")
+    
+    print(scan_db)
+    print(scan_update.json())
+    scan_db = db.scans.update_one(query, {'$set': scan_update.json()})
+    print(scan_db)
+    return Scan(id=scan_id, **scan_db)
